f235b8a94a3d5248acdf95d896f9f4e76e71d7ba
akirahashibami <hashibami2207@gmail.com>
2020-11-10 07:44:25 +0000

    re

--- db/migrate/20201110071913_change_data_relationships_to_followe_id.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataRelationshipsToFolloweId < ActiveRecord::Migration[5.2]
-  def change
-    change_column :relationships, :following_id, :'integer USING CAST(column_name AS integer)'
-    change_column :relationships, :follower_id, :'integer USING CAST(column_name AS integer)'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_071913) do
+ActiveRecord::Schema.define(version: 2020_11_10_073815) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
